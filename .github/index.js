c600e6e283af263f8830ab451346e4edd88972c1
Iram Liu <64221371+ili23@users.noreply.github.com>
2020-09-20 21:42:06 -0400

    9/20 Class Notes
    Part 2 of JS series

---
+++ .github/index.js
@@ -0,0 +1,22 @@
+//OLD WAY OF DECLARING
+
+//variable with number
+var life = 100;
+
+life = life / 10;
+
+//variable with text
+var name = 'Name' //Has to use '' for text
+
+var checkout = true; //BOOLEAN
+
+var random; //if you don't declare value, this variable will be undefined
+
+var random1 = null; //null means that the variable has no value
+
+//NEW WAY OF DECLARING
+const life = 100; //const doesn't change since it is constant and you cannot redeclare
+
+life = 50; //let allows you to change or redeclare
+
+console.log('Hello World'); 
